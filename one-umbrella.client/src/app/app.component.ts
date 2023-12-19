df66b2d18ae869fcfd326666a30730068d35c75b
Julien Noel <juliennoel998@gmail.com>
2023-12-19 22:08:28 +0100

    tailwind/navbar/-index issue

--- one-umbrella.client/src/app/app.component.ts
+++ one-umbrella.client/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { CommonModule, DatePipe } from '@angular/common';
 import { Router, RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, FormsModule, ReactiveFormsModule, HttpClientModule, RouterModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, FormsModule, ReactiveFormsModule, HttpClientModule, RouterModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   providers: []
